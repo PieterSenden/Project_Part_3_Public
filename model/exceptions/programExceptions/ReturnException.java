8feeab938f9d57bd029c6cd1c000963317961f7a
JorisCe <JorisCe@users.noreply.github.com>
2017-05-14 22:08:12 +0200

    Added ReturnException

---
+++ model/exceptions/programExceptions/ReturnException.java
@@ -0,0 +1,32 @@
+package asteroids.model.exceptions.programExceptions;
+
+import be.kuleuven.cs.som.annotate.*;
+
+/**
+ * A class for signaling return statements.
+ * 
+ * @author Joris Ceulemans & Pieter Senden
+ * @version 3.0
+ * 
+ * @note This exception may only be caught during the execution of a function and thrown in a ReturnStatement. 
+ */
+
+
+public class ReturnException extends RuntimeException {
+	/**
+	 * Initialize this new ReturnException
+	 */
+	public ReturnException(Object returnValue) {
+		super();
+		this.returnValue = returnValue;
+	}
+	
+	@Basic
+	public Object getReturnValue() {
+		return returnValue;
+	}
+	
+	private final Object returnValue;
+	
+	private static final long serialVersionUID = 1L;
+}
