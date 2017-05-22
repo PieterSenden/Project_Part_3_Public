c3b63fbca9e96c51157354482e5f547c26efd242
JorisCe <JorisCe@users.noreply.github.com>
2017-05-22 11:38:21 +0200

    Debugging 21-22/05 Morning

---
+++ model/programs/expressions/BulletExpression.java
@@ -0,0 +1,33 @@
+package asteroids.model.programs.expressions;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import asteroids.model.exceptions.IllegalMethodCallException;
+import asteroids.model.programs.ProgramExecutor;
+import asteroids.model.representation.Bullet;
+
+public class BulletExpression extends Expression<Bullet> {
+	
+	public Stream<Bullet> stream(ProgramExecutor executor) throws IllegalMethodCallException {
+		if (executor == null || executor.getShip() == null || executor.getShip().getFiredBullets() == null)
+			throw new IllegalMethodCallException();
+		Stream.Builder<Bullet> builder = Stream.builder();
+		for (Bullet bullet : executor.getShip().getFiredBullets())
+			builder.accept(bullet);
+		return builder.build();
+	}
+	
+	@Override
+	public Bullet evaluate(ProgramExecutor executor) throws IllegalMethodCallException {
+		if (executor == null || executor.getShip() == null)
+			throw new IllegalMethodCallException();
+		if (executor.getShip().getFiredBullets().isEmpty())
+			return null;
+		Stream<Bullet> bulletStream = stream(executor);
+		Optional<Bullet> result = bulletStream.filter(b -> !b.isTerminated())
+											.reduce((b1,b2) -> b1);
+		return result.isPresent() ? result.get() : null;
+	}
+	
+}
